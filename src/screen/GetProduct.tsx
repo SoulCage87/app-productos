4f8aeab04b837e93325ab0ffd6054a90eb0874ba
Samuel Galo <samuelgalo1625@gmail.com>
2024-06-22 11:18:26 -0600

    final

--- src/screen/GetProduct.tsx
+++ src/screen/GetProduct.tsx
@@ -1,11 +1,72 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Button, ScrollView, Image, Alert } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
+import { DataTable } from 'react-native-paper';
 
 const GetProduct = () => {
+
+  const [productos, setProductos] = useState([]);
+  const [show, setShow] = useState(false);
+  const [selectedProduct, setSelectedProduct] = useState<any>(null);
+  const [eliminate, setEliminate] = useState(false)
+
+  const fetchProducts = async () => {
+    try {
+      const fetch = await axios.get(`http://192.168.100.7:3000/api/producto`);
+      setProductos(fetch.data);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  const dltProducts = async (id: number) => {
+    setEliminate(true)
+    try {
+      const fetch = await axios.delete(`http://192.168.100.7:3000/api/producto/${id}`);
+      fetchProducts();
+      Alert.alert('Producto eliminado con Exito!')
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  const detail = (producto: any) => {
+    setSelectedProduct(producto);
+    setShow(true);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, [])
+
   return (
-    <View>
-      <Text>GetProduct</Text>
-    </View>
+    <ScrollView>
+      <DataTable>
+        <DataTable.Header>
+          <DataTable.Title>Nombre</DataTable.Title>
+          <DataTable.Title>Precio</DataTable.Title>
+          <DataTable.Title>Ver</DataTable.Title>
+        </DataTable.Header>
+        {productos.map((producto: any) => (
+          <DataTable.Row key={producto.id}>
+            <DataTable.Cell>{producto.nombre}</DataTable.Cell>
+            <DataTable.Cell>{producto.precio}</DataTable.Cell>
+            <DataTable.Cell><Button title='Ver' onPress={() => detail(producto)}></Button></DataTable.Cell>
+          </DataTable.Row>
+        ))}
+      </DataTable>
+
+      {show && selectedProduct && (
+        <View style={{ marginTop: 10 }}>
+          <Text style={{ alignSelf: 'center' }}>Nombre: {selectedProduct.nombre}</Text>
+          <Text style={{ alignSelf: 'center' }}>Precio: {selectedProduct.precio}</Text>
+          <Text style={{ alignSelf: 'center' }}>Descripcion: {selectedProduct.descripcion}</Text>
+          <Button title='Eliminar' onPress={() => dltProducts(selectedProduct.id)}></Button>
+        </View>
+      )}
+
+
+    </ScrollView>
   )
 }
 
--- src/screen/PostProducts.tsx
+++ src/screen/PostProducts.tsx
@@ -1,17 +1,19 @@
-import { View, Text, StyleSheet, Button, TextInput, Image, Alert } from 'react-native'
+import { View, Text, StyleSheet, Button, TextInput, Image, Alert, TouchableOpacity } from 'react-native'
 import React, {useState} from 'react'
 import * as ImagePicker from 'expo-image-picker';
 import * as FileSystem from 'expo-file-system';
 import { Picker } from '@react-native-picker/picker';
 import axios from 'axios';
+import { NavigationProp } from '@react-navigation/native';
 
-const PostProducts = () => {
+const PostProducts = ({navigation}: { navigation: NavigationProp<any> }) => {
 const [photo, setPhoto] = useState(null);
 const [nombre, setNombre] = useState('');
 const [descripcion, setDescripcion] = useState('');
 const [precio, setPrecio] = useState('');
 const [categoria, setCategoria] = useState('');
-const [estado, setEstado] = useState('');
+const [estado, setEstado] = useState('Disponible');
+
 
 const pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -23,36 +25,47 @@ const pickImage = async () => {
     });
 
     if (!result.canceled) {
-        setPhoto(result.uri);
+        setPhoto(result.assets[0].uri);
     }
   };
 
-  const uploadimg = async () => {
+   const uploadimg = async () => {
     if (photo) {
-      const file = await FileSystem.readAsStringAsync(photo, {
-        encoding: FileSystem.EncodingType.Base64,
+      const formData = new FormData();
+      formData.append('nombre', nombre);
+      formData.append('descripcion', descripcion);
+      formData.append('precio', precio);
+      formData.append('estado', estado === 'Disponible' ? 'Disponible' : 'No disponible')
+      formData.append('categoria', categoria);
+
+      const uriParts = photo.split('.');
+      const fileType = uriParts[uriParts.length - 1];
+      formData.append('foto', {
+        uri: photo,
+        name: `photo.${fileType}`,
+        type: `image/${fileType}`,
       });
-      const filename = photo.split('/').pop();
-      const mimetype = 'image/jpeg'; 
-  
+
       try {
-        const response = await axios.post('http://192.168.100.7:3000/api/producto', {
-          nombre,
-          descripcion,
-          precio,
-          estado,
-          categoria,
-          foto: file,
-          nombre_archivo: filename,
-          mime_type: mimetype,
+        const response = await axios.post('http://192.168.100.7:3000/api/producto', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
         });
         console.log(response.data);
         Alert.alert('Subido Con Exito!');
-      } catch (error) {
-        console.error('Error uploading image:', error);
+      } catch (error: any) {
+        console.error('Error uploading image:', error.response.data);
+        Alert.alert('Error al subir la imagen:', error.message);
       }
+    } else {
+      Alert.alert('Por favor seleccione una imagen primero');
     }
   };
+
+  const handleProducts = () => {
+    navigation.navigate('GetProduct');
+  }
 
 
   return (
@@ -92,13 +105,16 @@ const pickImage = async () => {
 
         {photo ? (
         <Image
-          style={{ width: 200, height: 200, marginTop: 20 }}
+          style={{ width: 200, height: 200, marginTop: 20, alignSelf: 'center', marginEnd: 10 }}
           source={{ uri: photo }}
         />
       ) : null}
     
   <Button title='Subir Informacion' onPress={uploadimg}></Button>
 
+<TouchableOpacity style={styles.detail}>
+  <Button title='Detalle item' onPress={() => navigation.navigate('GetProduct')}></Button>
+</TouchableOpacity>
     </View>
   )
 }
@@ -116,6 +132,13 @@ const styles = StyleSheet.create({
       width: '100%',
       marginBottom: 10,
     },
+    detail: {
+      borderColor: 'black',
+      alignSelf: 'center',
+      fontSize: 20,
+      fontWeight: '100',
+      top: 70
+    }
   });
 
 export default PostProducts
